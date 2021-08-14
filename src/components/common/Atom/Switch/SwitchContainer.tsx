415f76675a3a5beb9d1e63bd651cd1c364066453
upship-kim <tkdqook@gmail.com>
2021-08-14 16:51:00 +0900

    스위치 컴포넌트 컨테이너 생성

--- src/components/common/Atom/Switch/SwitchContainer.tsx
+++ src/components/common/Atom/Switch/SwitchContainer.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import SwitchForm from './SwitchForm';
 
 const SwitchContainer = () => {
     const [state, setState] = useState<boolean>(false);
@@ -8,44 +8,7 @@ const SwitchContainer = () => {
         setState(() => !state);
     };
 
-    return (
-        <Container style={state ? {} : { opacity: '0.8' }}>
-            <Wrapper
-                onClick={onChange}
-                style={state ? {} : { justifyContent: 'flex-end' }}
-            >
-                <Circle />
-            </Wrapper>
-        </Container>
-    );
+    return <SwitchForm state={state} onChange={onChange} />;
 };
-
-const Container = styled.div`
-    display: flex;
-    opacity: 0.4;
-    width: 4rem;
-    height: 2rem;
-    padding: 0.3rem;
-    border-radius: 1.2rem;
-    background: ${props => props.theme.bg};
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-`;
-const Wrapper = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-    align-items: center;
-    border-radius: 1rem;
-    background: ${props => props.theme.navBg};
-`;
-const Circle = styled.div`
-    width: 1.5rem;
-    height: 1.5rem;
-    margin: 0.3rem;
-    border-radius: 100%;
-    background: ${props => props.theme.bg};
-`;
 
 export default SwitchContainer;
---
+++ src/components/common/Atom/Switch/SwitchForm.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type SwitchType = {
+    state: boolean;
+    onChange: () => void;
+};
+
+const SwitchForm = ({ state, onChange }: SwitchType) => {
+    return (
+        <Container style={state ? {} : { opacity: '0.8' }}>
+            <Wrapper
+                onClick={onChange}
+                style={state ? {} : { justifyContent: 'flex-end' }}
+            >
+                <Circle />
+            </Wrapper>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    display: flex;
+    opacity: 0.4;
+    width: 4rem;
+    height: 2rem;
+    padding: 0.3rem;
+    border-radius: 1.2rem;
+    background: ${props => props.theme.bg};
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+`;
+const Wrapper = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    border-radius: 1rem;
+    background: ${props => props.theme.navBg};
+`;
+const Circle = styled.div`
+    width: 1.5rem;
+    height: 1.5rem;
+    margin: 0.3rem;
+    border-radius: 100%;
+    background: ${props => props.theme.bg};
+`;
+
+export default SwitchForm;
