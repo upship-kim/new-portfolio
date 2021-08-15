98e293a551d9e93cdf2a1c0c08cbbb083efb4a56
upship-kim <tkdqook@gmail.com>
2021-08-15 16:53:10 +0900

    contents page 생성

--- src/components/Templates/ContentsTemplate/index.tsx
+++ src/components/Templates/ContentsTemplate/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
+import Contact from '../../../pages/Contact';
 import Home from '../../../pages/Home';
 import Portfolio from '../../../pages/Portfolio';
+import Resume from '../../../pages/Resume';
 const index = () => {
     return (
         <Container>
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/portfolio" component={Portfolio} />
+                <Route exact path="/resume" component={Resume} />
+                <Route exact path="/contact" component={Contact} />
             </Switch>
         </Container>
     );
---
+++ src/pages/Resume.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Resume = () => {
+    return <h1> this is Resume Page~!</h1>;
+};
+
+export default Resume;
